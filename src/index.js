f0cf7db3e6d54ee0a1c5641357d9988c72589643
Octa <octavio.macieira@aarim.com>
2022-07-27 18:35:41 -0300

    Application of class

--- src/index.js
+++ src/index.js
@@ -1,14 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const App = () => {
-    window.navigator.geolocation.getCurrentPosition(
-        position => console.log(position),
-        err => console.log(err)
-    );
+// const App = () => {
 
-    return <div> Hi there!</div>
+//     );
+
+//     return <div>Latitude: </div>
+// }
+
+class App extends React.Component
+{
+    render()
+    {
+        window.navigator.geolocation.getCurrentPosition(
+            position => console.log(position),
+            err => console.log(err)
+        );
+
+        return <div>Latitude: </div>
+    }
 }
+
 
 ReactDOM.render(
     <App />,
