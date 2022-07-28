b3698e7f8a1abd7e0bcfa21272aa7f2db58fd56f
Octa <octavio.macieira@aarim.com>
2022-07-28 17:00:37 -0300

    Add Constructor

--- src/index.js
+++ src/index.js
@@ -10,6 +10,14 @@ import ReactDOM from "react-dom";
 
 class App extends React.Component
 {
+    constructor(props)
+    {
+        super(props);
+
+        this.state = {lat: null};
+    }
+
+    //React says we have to define render!!
     render()
     {
         window.navigator.geolocation.getCurrentPosition(
