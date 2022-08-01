ced586c5584efa8a4a5e1bf2fc857381b11b031b
Octa <octavio.macieira@aarim.com>
2022-08-01 17:32:29 -0300

    Improving the Season functionality

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,7 +1,29 @@
 import React from "react";
 
+const getSeason = (lat, month) =>{
+        if (month > 2 && month < 9)
+        {
+            return lat > 0 ? 'summer' : 'winter';
+        }
+        else
+        {
+                return lat > 0 ? 'winter' : 'summer';
+        }
+        
+}
+
 const SeasonDisplay = (props) => {
-        return <div>Season Display</div>
+        const season = getSeason(props.lat, new Date().getMonth());
+        const text = season === 'winter' ? 'Buur, it is chilly' : 'Let`s hit the beach!';
+        const icon = season === 'winter' ? 'snowflake' : 'sun';
+
+        return (
+                <div>
+                        <i className={`${icon} icon`} />
+                        <h1>{text}</h1>
+                        <i className={`${icon} icon`} />
+                </div>
+        );
 };
 
 export default SeasonDisplay;
